b5763b72cadbc268b07df495bbc01bfe5a365d64
Punit <sharmapunit1622006@gmail.com>
2026-01-29 08:02:22 +0530

    83.

---
+++ removDuplicatesfromSortedList.java
@@ -0,0 +1,52 @@
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int val) {
+        this.val = val;
+    }
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        ListNode current = head;
+        while (current != null && current.next != null) {
+            if (current.val == current.next.val) {
+                current.next = current.next.next;
+            } else {
+                current = current.next;
+            }
+        }
+        return head;
+    }
+
+    public static void main(String[] args) {
+        // Create a sorted linked list with duplicates: 1 -> 1 -> 2 -> 3 -> 3
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(1);
+        head.next.next = new ListNode(2);
+        head.next.next.next = new ListNode(3);
+        head.next.next.next.next = new ListNode(3);
+
+        System.out.println("Original list:");
+        printList(head);
+
+        Solution solution = new Solution();
+        ListNode result = solution.deleteDuplicates(head);
+
+        System.out.println("After removing duplicates:");
+        printList(result);
+    }
+
+    public static void printList(ListNode head) {
+        ListNode current = head;
+        while (current != null) {
+            System.out.print(current.val + " -> ");
+            current = current.next;
+        }
+        System.out.println("null");
+    }
+}
