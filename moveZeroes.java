f938bc549c35017c841d149c858fe620cf4547f8
Punit <sharmapunit1622006@gmail.com>
2026-02-05 16:10:20 +0530

    283.

---
+++ moveZeroes.java
@@ -0,0 +1,24 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int index =0;
+        for(int i =0; i< nums.length;i++){
+            if(nums[i]!=0){
+            nums[index]=nums[i];
+            index++;
+            
+            }
+        }
+            for(int i = index; i<nums.length;i++){
+                nums[i] = 0;
+            
+
+        }
+        
+    }
+}
+public static void main(String[] args) {
+    Solution solution = new Solution();
+    int[] nums = {0,1,0,3,12};
+    solution.moveZeroes(nums);
+    System.out.println(Arrays.toString(nums));
+}
